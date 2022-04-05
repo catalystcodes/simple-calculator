8c47d3518274288c0486b80438339c10b01a84d6
muhammad <muh4mub@gmail.com>
2022-04-05 15:26:07 +0100

    update datebase

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ class Calculator {
 	}
 
 	delete() {
-
+		this.currentOperand = this.currentOperand.toString().slice(0, -1)
 	}
 
 	appendNumber(number) {
@@ -51,11 +51,37 @@ class Calculator {
 			default:
 				return
 		}
+		this.currentOperand = computation
+		this.operation = undefined
+		this.previousOperand = ''
+	}
+
+	getDisplayNumber(number) {
+		const stringNumber = number.toString()
+		const intergersDigits = Number(stringNumber.split('.')[0])
+		const decimalDigits = stringNumber.split('.')[1]
+		let intergerDisplay
+		if(isNaN(intergersDigits)) {
+			intergerDisplay = ''
+		} else {
+			intergerDisplay = intergersDigits.toLocaleString('en', {maximumFractionDigits:0 })
+		}
+		if(decimalDigits != null) {
+			return `${intergerDisplay}.${decimalDigits}`
+		} else {
+			return intergerDisplay
+		}
 	}
 
 	updateDisplay() {
-		this.currentOperandTextElement.innerText = this.currentOperand
-		this.previousOperandTextElement.innerText = this.previousOperand
+		this.currentOperandTextElement.innerText = 
+		this.getDisplayNumber(this.currentOperand)
+		if(this.operation != null) {
+			this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)
+			} ${this.operation}`
+		} else {
+			this.previousOperandTextElement.innerText = ''
+		}
 	}
 }
 
@@ -63,7 +89,8 @@ class Calculator {
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
-const allClearButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const deleteButton = document.querySelector('[data-delete]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
@@ -86,5 +113,15 @@ operationButtons.forEach(button => {
 
 equalsButton.addEventListener('click', button => {
 	calculator.compute()
+	calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+	calculator.clear()
+	calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+	calculator.delete()
 	calculator.updateDisplay()
 })
